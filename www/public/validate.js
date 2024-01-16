1ec087e2b67b9955d63e6ad7a59c537df796bfbb
melketesfay <melketesfay@gmail.com>
2024-01-17 00:19:05 +0100

    working js test validation

--- www/public/validate.js
+++ www/public/validate.js
@@ -1,7 +1,13 @@
 let radios = document.querySelectorAll("input[type='radio']");
 
+let radioArr = Array.from(radios);
+// let test = radios.some((e) => e.checked == true);
+
+console.log(typeof radioArr);
+
 function checkInput() {
-  if (radios[0].checked == false) {
+  let test = radioArr.every((e) => e.checked == false);
+  if (test) {
     alert("stop");
 
     return false;
