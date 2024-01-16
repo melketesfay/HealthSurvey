ed842270b3933ff836692a28a573d82dab931844
melketesfay <melketesfay@gmail.com>
2024-01-16 11:13:09 +0100

    changed index.php to T-index.php and changed input fields

--- www/public/quiz.php
+++ www/public/quiz.php
@@ -1,6 +1,6 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-   
+
     session_start();
 }
 
@@ -52,79 +52,127 @@ oder sind schon fertig zubereitet?"];
 
                         echo "   
                  
-  <label for='question_1'>$USERDATA[0]</label>
+  <p for='question_1'>$USERDATA[0]</p>
   </fieldset>
   <fieldset class='fieldset_input'>
-  <input type='text' name='question_1' />
+  <label for='question_1'>1:Überhaupt nicht gesund 5: Extrem Gesund</label>
+  <input type='range' min='1' max='5' value='3' class='slider' id='myRange' name='question_1'>
+  
+ 
 
    ";
                     }
-                  if(isset($_GET['question_1'])){
-                    $_SESSION['number1'] = $_GET['question_1'];
+                    if (isset($_GET['question_1'])) {
+                        $_SESSION['number1'] = $_GET['question_1'];
                         echo "   
-    <label for='question_2'>$USERDATA[1]</label>
+    <p for='question_2'>$USERDATA[1]</p>
     </fieldset>
-    <fieldset class='fieldset_input'>
-    <input type='text' name='question_2' />
+    <fieldset class='fieldset_input' >
+    <label for='question_2'>ja</label>
+
+    <input type='radio'  name='question_2' />
+    <label for='question_2_1'>nein</label>
+    <input type='radio'  name='question_2_1' />
    
     
     ";
                     }
-                    if (isset($_GET['question_2'])) {
-                        $_SESSION['number2'] = $_GET['question_2'];
+                    if (isset($_GET['question_2']) || isset($_GET['question_2_1'])) {
+                        $_SESSION['number2'] = $_GET['question_2'] ?? $_GET['question_2_1'];
                         echo "   
-    <label for='question_3'>$USERDATA[2]</label>
+    <p for='question_3'>$USERDATA[2]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
-    <input type='text' name='question_3' />
+    
+    <label for='question_3'>1:Überhaupt nicht wichtig 5: Sehr Wichtig</label>
+    <input type='range' min='1' max='5' value='3' class='slider' id='myRange' name='question_3'>
+
+
+
+
    
   
     ";
+
+                        // Keine zusätzliche körperliche Aktivität /
+                        // Gewichte heben / Gehen / Wandern / Joggen /
+                        // Rennen / Schwimmen / Tanzen / Aerobics /
+                        // Pilates / Team Sport / Andere (Radio Buttons
+                        // oder Checkboxen; “Andere” zusätzlich mit
+                        // einzeiligem Eingabefeld; OK-Button)
+
                     }
                     if (isset($_GET['question_3'])) {
                         $_SESSION['number3'] = $_GET['question_3'];
                         echo "  
-    <label for='question_4'>$USERDATA[3]</label>
+    <p for='question_4'>$USERDATA[3]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
-    <input type='text' name='question_4' />
+    <label for='question_4'>Keine zusätzliche körperliche Aktivität</label>
+
+    <input type='radio'  name='question_4' />
+    <label for='question_4_1'>Gewichte heben</label>
+    <input type='radio'  name='question_4_1' />
+    <label for='question_4_2'>Gehen</label>
+    <input type='radio'  name='question_4_2' />
+    <label for='question_4_4'>Wandern</label>
+    <input type='radio'  name='question_4_4' />
+    <label for='question_4_4'>Joggen</label>
+    <input type='radio'  name='question_4_4' />
+    <label for='question_4_5'>Rennen</label>
+    <input type='radio'  name='question_4_5' />
+    <label for='question_4_6'>Schwimmen</label>
+    <input type='radio'  name='question_4_6' />
+    <label for='question_4_7'>Tanzen</label>
+    <input type='radio'  name='question_4_7' />
+    <label for='question_4_8'>Aerobics</label>
+    <input type='radio'  name='question_4_8' />
+    <label for='question_4_9'>Pilates</label>
+    <input type='radio'  name='question_4_9' />
+    <label for='question_4_10'>Team Sport</label>
+    <input type='radio'  name='question_4_10' />
+    <label for='question_4_11'>Andere</label>
+    <input type='checkbox'  name='question_4_11' />
     
    ";
                     }
                     if (isset($_GET['question_4'])) {
                         $_SESSION['number4'] = $_GET['question_4'];
                         echo "   
-    <label for='question_5'>$USERDATA[4]</label>
+    <p for='question_5'>$USERDATA[4]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
-    <input type='text' name='question_5' />
+    <label for='question_5'>1:Zu wenig 5: Zu viel</label>
+    <input type='range' min='1' max='5' value='3' class='slider' id='myRange' name='question_5'>
    
     ";
                     }
                     if (isset($_GET['question_5'])) {
                         $_SESSION['number5'] = $_GET['question_5'];
                         echo "   
-    <label for='question_6'>$USERDATA[5]</label>
+    <p for='question_6'>$USERDATA[5]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
-    <input type='text' name='question_6' />
+    <label for='question_6'>Gib eine zahl ein</label>
+    <input type='number' name='question_6' />
    
   ";
                     }
                     if (isset($_GET['question_6'])) {
                         $_SESSION['number6'] = $_GET['question_6'];
                         echo "   
-    <label for='question_7'>$USERDATA[6]</label>
+    <p for='question_7'>$USERDATA[6]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
-    <input type='text' name='question_7' />
+    <label for='question_7'>Gib eine zahl ein</label>
+    <input type='number' name='question_7' />
    
     ";
                     }
                     if (isset($_GET['question_7'])) {
                         $_SESSION['number7'] = $_GET['question_7'];
                         echo "   
-    <label for='question_8'>$USERDATA[7]</label>
+    <p for='question_8'>$USERDATA[7]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
     <input type='text' name='question_8' />
@@ -135,7 +183,7 @@ oder sind schon fertig zubereitet?"];
                     if (isset($_GET['question_8'])) {
                         $_SESSION['number8'] = $_GET['question_8'];
                         echo "  
-    <label for='question_9'>$USERDATA[8]</label>
+    <p for='question_9'>$USERDATA[8]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
     <input type='text' name='question_9' />
@@ -146,7 +194,7 @@ oder sind schon fertig zubereitet?"];
                     if (isset($_GET['question_9'])) {
                         $_SESSION['number9'] = $_GET['question_9'];
                         echo "   
-    <label for='question_10'>$USERDATA[9]</label>
+    <p for='question_10'>$USERDATA[9]</p>
     </fieldset>
     <fieldset class='fieldset_input'>
     <input type='text' name='question_10' />
@@ -162,10 +210,10 @@ oder sind schon fertig zubereitet?"];
 
                     ?>
                     <!--  -->
-                    </fieldset>
-                    <fieldset class='fieldset_buttons'> 
+                </fieldset>
+                <fieldset class='fieldset_buttons'>
                     <button onclick='window.history.go(-1); return false;' class='previous_button'>Previous</button>
-                    <input class='next_button' type='submit' value='Next' name="submit"/>
+                    <input class='next_button' type='submit' value='Next' name="submit" />
                 </fieldset>
 
             </form>
