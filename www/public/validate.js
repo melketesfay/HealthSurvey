70915ee33e63b657acc8793e347a9d4307a1aca7
melketesfay <melketesfay@gmail.com>
2024-01-17 17:37:03 +0100

    before merge

--- www/public/validate.js
+++ www/public/validate.js
@@ -3,11 +3,12 @@ let radios = document.querySelectorAll("input[type='radio']");
 let radioArr = Array.from(radios);
 // let test = radios.some((e) => e.checked == true);
 
-console.log(typeof radioArr);
+console.log(radioArr);
+console.log(radioArr.length);
 
 function checkInput() {
   let test = radioArr.every((e) => e.checked == false);
-  if (test) {
+  if (test && radioArr.length > 0) {
     alert("stop");
 
     return false;
