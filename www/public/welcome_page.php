f6a473bdd41924b340c7dad607fcd3e326e71435
melketesfay <melketesfay@gmail.com>
2024-01-18 14:45:09 +0100

    copied welcomepage

--- www/public/welcome_page.php
+++ www/public/welcome_page.php
@@ -18,21 +18,30 @@ if (session_status() === PHP_SESSION_NONE) {
 <body>
     <div class="form_container">
         <div class="wellcome_message form_index">
-            <h1>Wellcome <?php echo $_SESSION['username'];  ?></h1>
-            <p>Thrilled to have you on board! <span style="font-size: 1.5em;">🌟</span> Let's kickstart your health journey. Your well-being is our focus, and this survey is your first step in understanding and optimizing your health.</p>
+            <div class="div_welcome_text">
+                <h1>Wellcome <?php echo $_SESSION['username'];  ?></h1>
+                <p>Thrilled to have you on board! <span style="font-size: 1.5em;">🌟</span> Let's kickstart your health journey. Your well-being is our focus, and this survey is your first step in understanding and optimizing your health.</p>
 
-            <p>📋 Answer our thoughtful survey questions, and unlock personalized recommendations. Empower yourself with informed choices and take control of your well-being.</p>
+                <p>📋 Answer our thoughtful survey questions, and unlock personalized recommendations. Empower yourself with informed choices and take control of your well-being.</p>
 
-            <p>Your health is in your hands. Together, let's work towards a healthier, happier you! Questions or need assistance? We're here for you.</p>
+                <p>Your health is in your hands. Together, let's work towards a healthier, happier you! Questions or need assistance? We're here for you.</p>
 
-            <p>Cheers to your health and wellness! <span style="font-size: 1.5em;">🌈</span> The Health Survey Team</p>
-
+                <p>Cheers to your health and wellness! <span style="font-size: 1.5em;">🌈</span> The Health Survey Team</p>
+            </div>
         </div>
-        <div class="wellcome_button ">
-            <form action="quiz.php" method="get">
-                <input type="submit" class="next_button" name="start" value="to quiz">
-            </form>
+        <div class="c_wellcome_button">
+            <div class="wellcome_button ">
+                <form action="index.php" method="get">
+                    <input type="submit" class="previous_button" name="start" value="back">
+                </form>
+            </div>
+            <div class="wellcome_button ">
+                <form action="quiz.php" method="get">
+                    <input type="submit" class="next_button" name="start" value="to quiz">
+                </form>
+            </div>
         </div>
+
     </div>
 
 
