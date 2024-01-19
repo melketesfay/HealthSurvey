7e0beaed75ae62bdd19be3b1cdfd9a726f948230
melketesfay <melketesfay@gmail.com>
2024-01-19 16:24:33 +0100

    fixed a small bug on the header of welcome_page

--- www/public/welcome_page.php
+++ www/public/welcome_page.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <link rel="stylesheet" href="wireframe.css">
 </head>
